bb074f4b8fa2479931a8ceddf2b3627c14443afd
alvarezaaronai <alvarez.a.aaron@gmail.com>
2018-04-21 23:59:54 -0700

    Added a new Activity Google Maps

--- app/src/main/java/sample/android/example/com/simpleapp/SecondActivity.java
+++ app/src/main/java/sample/android/example/com/simpleapp/SecondActivity.java
@@ -17,6 +17,7 @@ public class SecondActivity extends AppCompatActivity {
         Toast.makeText(this, "User" + i, Toast.LENGTH_SHORT).show();
         Button bt2 = findViewById(R.id.btClickedMe);
         Button bt3 = findViewById(R.id.btGoBack);
+        Button bt4 = findViewById(R.id.btMaps);
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +33,13 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent int3 = new Intent(SecondActivity.this,MainActivity.class);
                 startActivity(int3);
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int4 = new Intent(SecondActivity.this,GoogleMapsActivity.class);
+                startActivity(int4);
             }
         });
     }
